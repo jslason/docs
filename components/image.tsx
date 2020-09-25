a8c26da3ac578ef7730ae2caacc62ed02f299b5f
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-09-25 14:06:50 +0800

    Add a border and box shadow to all images

--- components/image.tsx
+++ components/image.tsx
@@ -1,11 +1,21 @@
 import useBaseUrl from '@docusaurus/useBaseUrl' 
 import React from 'react'
 
+const imgStyle = {
+  border: '1px solid #eee',
+  boxShadow: '0 0 15px rgba(0, 0, 0, 0.1)',
+  margin: '15px 15px 0 15px'
+}
+
 export default function Image ({alt, src}) {
   return (
     <p>
       <div style={{textAlign: 'center'}}>
-        <img alt={alt} src={useBaseUrl(src)} />
+        <img 
+          alt={alt} 
+          src={useBaseUrl(src)} 
+          style={imgStyle}
+        />
         <div 
           style={{
             fontStyle: 'italic',
