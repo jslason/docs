1976ac27b5d94c33fe1747eab78cbfdefc806783
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-10-05 10:16:44 +0800

    Update more images and video

---
+++ components/video.tsx
@@ -0,0 +1,29 @@
+import useBaseUrl from '@docusaurus/useBaseUrl' 
+import React from 'react'
+
+const imgStyle = {
+  // border: '1px solid #eee',
+  boxShadow: '0 0 15px rgba(0, 0, 0, 0.2)',
+  margin: '15px 0 0',
+  maxWidth: '100%'
+}
+
+export default function Video ({alt, src}) {
+  return (
+    <p>
+      <div style={{textAlign: 'center'}}>
+        <video 
+          controls
+          src={useBaseUrl(src)} 
+          style={imgStyle}
+        />
+        <div 
+          style={{
+            fontStyle: 'italic',
+            fontSize: '75%'
+          }}
+        >{alt}</div>
+      </div>
+    </p>
+  )
+}
