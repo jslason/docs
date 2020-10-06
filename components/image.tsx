3b691dfb391b0f4b05336d0fe5da3f192e775c4e
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-10-06 13:12:14 +0800

    Grow images on click and hold instead of hover

--- components/image.tsx
+++ components/image.tsx
@@ -1,19 +1,13 @@
 import useBaseUrl from '@docusaurus/useBaseUrl' 
 import React from 'react'
 
-const imgStyle = {
-  boxShadow: '0 0 15px rgba(0, 0, 0, 0.2)',
-  margin: '15px 0 0'
-}
-
 export default function Image ({alt, src}) {
   return (
     <div style={{marginBottom: '20px', textAlign: 'center'}}>
       <img 
         alt={alt} 
         className='GrowImage'
-        src={useBaseUrl(src)} 
-        style={imgStyle}
+        src={useBaseUrl(src)}
       />
       <div 
         style={{
