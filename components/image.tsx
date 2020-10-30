77cfa61b43738187772ce22e13a490b2408310b4
Trevor Gerhardt <trevorgerhardt@gmail.com>
2020-10-30 08:52:44 +0800

    Show images full screen on click

--- components/image.tsx
+++ components/image.tsx
@@ -1,14 +1,25 @@
 import useBaseUrl from '@docusaurus/useBaseUrl' 
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Image ({alt, src}) {
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <div style={{marginBottom: '20px', textAlign: 'center'}}>
       <img 
         alt={alt} 
-        className='GrowImage'
+        className='ZoomableImage'
+        height='300px'
+        onClick={() => setIsActive(true)}
         src={useBaseUrl(src)}
       />
+      {isActive && (
+        <img
+          className='ImageOverlay'
+          onClick={() => setIsActive(false)}
+          src={useBaseUrl(src)}
+        />
+      )}
       <div 
         style={{
           fontStyle: 'italic',
